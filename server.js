f995248112ab3da891a4a096202aaa8d0a17a5ac
Aayush <nandkeolyar.aayush@gmail.com>
2018-03-06 01:34:59 +0530

    Change port config

--- server.js
+++ server.js
@@ -129,6 +129,6 @@ mongoose.connect(dbUrl, (err) => {
     console.log('mongo db connection', err)
 })
 
-var server = http.listen(3000, () => {
+var server = http.listen(process.env.PORT || 3000, () => {
     console.log('server is listening on port', server.address().port)
 })

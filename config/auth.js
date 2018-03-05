c4bdd10b117fa6c33b9053fd9e76abe8735e3e28
Aayush <nandkeolyar.aayush@gmail.com>
2018-03-06 01:40:22 +0530

    made callback url env variable

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : process.env.clientID,
         'clientSecret'  : process.env.clientSecret,
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : process.env.callbackURL,
     }
 };
